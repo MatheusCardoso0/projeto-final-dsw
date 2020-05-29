02e5e6b53e8c793cf825c1a7ed681b194ec5cbcf
MatheusCardoso0 <mathde12@gmail.com>
2020-05-29 19:43:32 -0300

    Alterações na página sobre

--- app/views/sobre.php
+++ app/views/sobre.php
@@ -140,112 +140,46 @@
   <!-- / menu -->
   <!-- catg header banner section -->
   <section id="aa-catg-head-banner">
-   <img src="img/fashion/fashion-header-bg-8.jpg" alt="fashion img">
-   <div class="aa-catg-head-banner-area">
-     <div class="container">
-      <div class="aa-catg-head-banner-content">
-        <h2>Contact</h2>
-        <ol class="breadcrumb">
-          <li><a href="index.php">Home</a></li>         
-          <li class="active">Contact</li>
-        </ol>
+    <img src="img/sobre.jpg" alt="fashion img">
+    <div class="aa-catg-head-banner-area">
+      <div class="container">
+        <div class="aa-catg-head-banner-content">
+          <h2>Sobre</h2>
+          <ol class="breadcrumb">
+            <li><a href="index.php">Home</a></li>
+            <li class="active">Sobre</li>
+          </ol>
+        </div>
       </div>
-     </div>
-   </div>
+    </div>
   </section>
   <!-- / catg header banner section -->
-<!-- start contact section -->
- <section id="aa-contact">
-   <div class="container">
-     <div class="row">
-       <div class="col-md-12">
-         <div class="aa-contact-area">
-           <div class="aa-contact-top">
-             <h2>We are wating to assist you..</h2>
-             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, quos.</p>
-           </div>
-           <!-- contact map -->
-           <div class="aa-contact-map">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3902.3714257064535!2d-86.7550931378034!3d34.66757706940219!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8862656f8475892d%3A0xf3b1aee5313c9d4d!2sHuntsville%2C+AL+35813%2C+USA!5e0!3m2!1sen!2sbd!4v1445253385137" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-           </div>
-           <!-- Contact address -->
-           <div class="aa-contact-address">
-             <div class="row">
-               <div class="col-md-8">
-                 <div class="aa-contact-address-left">
-                   <form class="comments-form contact-form" action="">
-                    <div class="row">
-                      <div class="col-md-6">
-                        <div class="form-group">                        
-                          <input type="text" placeholder="Your Name" class="form-control">
-                        </div>
-                      </div>
-                      <div class="col-md-6">
-                        <div class="form-group">                        
-                          <input type="email" placeholder="Email" class="form-control">
-                        </div>
-                      </div>
-                    </div>
-                     <div class="row">
-                      <div class="col-md-6">
-                        <div class="form-group">                        
-                          <input type="text" placeholder="Subject" class="form-control">
-                        </div>
-                      </div>
-                      <div class="col-md-6">
-                        <div class="form-group">                        
-                          <input type="text" placeholder="Company" class="form-control">
-                        </div>
-                      </div>
-                    </div>                  
-                     
-                    <div class="form-group">                        
-                      <textarea class="form-control" rows="3" placeholder="Message"></textarea>
-                    </div>
-                    <button class="aa-secondary-btn">Send</button>
-                  </form>
-                 </div>
-               </div>
-               <div class="col-md-4">
-                 <div class="aa-contact-address-right">
-                   <address>
-                     <h4>DailyShop</h4>
-                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum modi dolor facilis! Nihil error, eius.</p>
-                     <p><span class="fa fa-home"></span>Huntsville, AL 35813, USA</p>
-                     <p><span class="fa fa-phone"></span>+ 021.343.7575</p>
-                     <p><span class="fa fa-envelope"></span>Email: support@dailyshop.com</p>
-                   </address>
-                 </div>
-               </div>
-             </div>
-           </div>
-         </div>
-       </div>
-     </div>
-   </div>
- </section>
-
-  <!-- Subscribe section -->
-  <section id="aa-subscribe">
+  <!-- start contact section -->
+  <section id="aa-contact">
     <div class="container">
       <div class="row">
         <div class="col-md-12">
-          <div class="aa-subscribe-area">
-            <h3>Subscribe our newsletter </h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, velit!</p>
-            <form action="" class="aa-subscribe-form">
-              <input type="email" name="" id="" placeholder="Enter your Email">
-              <input type="submit" value="Subscribe">
-            </form>
+          <div class="aa-contact-area">
+            <div class="aa-contact-top">
+              <h2>Missão</h2>
+              <p>ajfgkjdasfhadshfiahdfiladhfiadhfilahdfiladhfliahdflihdailfhildahfiadhfiladhilfhdalifhiladhfiladhifhadifhladfiadhfdahlifhadlladhfladhflihadlifhdlifahosfjadsfjaidlfhadfldahflhadlkfjadçfjadoçfjaslçalçfjiefewjoeçrfjwçerfjçejfço</p>
+            </div>
+            <div class="aa-contact-top">
+              <h2>Visão</h2>
+              <p>ajfgkjdasfhadshfiahdfiladhfiadhfilahdfiladhfliahdflihdailfhildahfiadhfiladhilfhdalifhipfo</p>
+            </div>
+            <div class="aa-contact-top">
+              <h2>Valores</h2>
+              <p>ajfgkjdasfhadshfiahdfiladhfiadhfilahdfiladhfliahdflihdailfhildahfiadhfiladhilfhdalifhiladhfiladhifhadifhladfiadhfdahlifhadlladhfladhflihadlifhdlifahosfjadsfjaidlfhadfldahflhadlkfjadçfjadoçfjaslçalçfpfj pfj gj gaop jfaopj fgpoj gpfo</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </section>
-  <!-- / Subscribe section -->
 
- <!-- footer -->
- <footer id="aa-footer">
+  <!-- footer -->
+  <footer id="aa-footer">
     <!-- footer bottom -->
     <div class="aa-footer-top">
       <div class="container">
@@ -262,13 +196,13 @@
                       <li><a href="nao-alcoolica.php">Não Alcoólicas</a></li>
                       <li><a href="destilado.php">Destilados</a></li>
                       <li><a href="cerveja.php">Cervejas</a></li>
-                      
+
                   </div>
                 </div>
                 <div class="col-md-3 col-sm-6" style="margin-left: -90px; margin-top: 50px">
                   <div class="aa-footer-widget">
                     <div class="aa-footer-widget">
-                    <ul class="aa-footer-nav">
+                      <ul class="aa-footer-nav">
                         <li><a href="sobre.php">Sobre</a></li>
                         <li><a href="contact.ph">Contato</a></li>
                       </ul>
@@ -307,12 +241,12 @@
     </div>
   </footer>
   <!-- / footer -->
-  <!-- Login Modal -->  
+  <!-- Login Modal -->
   <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-      <div class="modal-content">                      
+      <div class="modal-content">
         <div class="modal-body">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
           <h4>Login</h4>
           <form class="aa-login-form" action="">
             <label for="">Email<span>*</span></label>
@@ -324,7 +258,7 @@
               Não tem uma conta?<a href="account.php">Registre-se</a>
             </div>
           </form>
-        </div>                        
+        </div>
       </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
   </div>
@@ -333,14 +267,14 @@
   <!-- jQuery library -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <!-- Include all compiled plugins (below), or include individual files as needed -->
-  <script src="js/bootstrap.js"></script>  
+  <script src="js/bootstrap.js"></script>
   <!-- SmartMenus jQuery plugin -->
   <script type="text/javascript" src="js/jquery.smartmenus.js"></script>
   <!-- SmartMenus jQuery Bootstrap Addon -->
-  <script type="text/javascript" src="js/jquery.smartmenus.bootstrap.js"></script>  
+  <script type="text/javascript" src="js/jquery.smartmenus.bootstrap.js"></script>
   <!-- To Slider JS -->
   <script src="js/sequence.js"></script>
-  <script src="js/sequence-theme.modern-slide-in.js"></script>  
+  <script src="js/sequence-theme.modern-slide-in.js"></script>
   <!-- Product view slider -->
   <script type="text/javascript" src="js/jquery.simpleGallery.js"></script>
   <script type="text/javascript" src="js/jquery.simpleLens.js"></script>
@@ -349,8 +283,9 @@
   <!-- Price picker slider -->
   <script type="text/javascript" src="js/nouislider.js"></script>
   <!-- Custom js -->
-  <script src="js/custom.js"></script> 
-  
+  <script src="js/custom.js"></script>
 
-  </body>
+
+</body>
+
 </html>
